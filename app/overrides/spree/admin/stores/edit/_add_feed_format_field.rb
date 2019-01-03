0b5768a9bc0066156501db14c62aa0dcd5c07de1
Jonathan Tapia <jonathan.tapia@magmalabs.io>
2019-01-03 13:35:23 -0600

    Remove unnecessary files

--- app/overrides/spree/admin/stores/edit/_add_feed_format_field.rb
+++
@@ -1,8 +0,0 @@
-Deface::Override.new(
-  virtual_path: 'spree/admin/stores/edit',
-  name: 'product_feeds_admin_edit',
-  insert_before: "erb[silent]:contains('if can? :update, @store')",
-  disabled: false,
-  partial: 'spree/admin/stores/feed_format_input',
-  original: 'ec1cf2b5f26ce7fd92ae1335e2f420ec94c2f4e6'
-)
