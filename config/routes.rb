8bc046c0976ea7c735f73e239d99461f1975ed9d
Jonathan Tapia <jonathan.tapia@magmalabs.io>
2019-01-10 11:24:49 -0600

    Update routes and locales

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,20 @@
 Spree::Core::Engine.routes.draw do
   namespace :admin do
-    resources :product_feeds
+    resources :product_feeds do
+      resources :images, only: [:create, :destroy], controller: :product_feed_images
+    end
+
     resources :product_catalogs do
       collection do
         get 'search'
+      end
+
+      member do
+        get 'list'
+      end
+
+      resources :variants, only: [] do
+        resources :images, only: [:edit, :update], controller: :product_catalog_images
       end
     end
   end
