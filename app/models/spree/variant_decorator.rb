d9a92522190e2b35998367d826558f4fe4d2cd0d
Jonathan Tapia <jonathan.tapia@magmalabs.io>
2019-01-17 00:51:13 -0600

    Add product feed images resource

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -1,4 +1,8 @@
 Spree::Variant.class_eval do
+  def product_feed_image
+    images.where(viewable_type: 'Spree::ProductCatalog').first
+  end
+
   def display_option_text
     options_text.present? && options_text || 'Master Variant'
   end
