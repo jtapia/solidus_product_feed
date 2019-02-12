1902d4632877dd1f555eee72f5b8400a9823aa09
Jonathan Tapia <jonathan.tapia@magmalabs.io>
2019-02-12 15:13:36 -0600

    Fix issue in case that the variant doesnt' have images

--- app/helpers/spree/admin/product_catalogs_helper.rb
+++ app/helpers/spree/admin/product_catalogs_helper.rb
@@ -10,13 +10,17 @@ module Spree
       def product_catalog_options(variant, product_catalog)
         attrs = JSON.parse(variant.to_json)
 
-        attrs[:image] = (variant.product_feed_image || variant.images.first).options
+        attrs[:image] = variant_images(variant)
         attrs[:slug] = variant.slug
         attrs[:name] = variant.name
         attrs[:is_master] = variant.display_option_text
         attrs[:is_selected] = product_catalog.selected?(variant.id)
 
         attrs
+      end
+
+      def variant_images(variant)
+        (variant.product_feed_image || variant.images.first).try(:options) || {}
       end
     end
   end
